dc5a80cbba85530aabd12541da7091b10e3f3ae8
Ashutosh <axxxtosh@gmail.com>
2018-05-02 13:56:07 +0530

    Home page 1.2

--- app/src/main/java/com/uriahsolution/homecare/ui/main/MainActivity.java
+++ app/src/main/java/com/uriahsolution/homecare/ui/main/MainActivity.java
@@ -1,42 +1,63 @@
 package com.uriahsolution.homecare.ui.main;
 
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.LinearLayout;
+import android.support.v7.widget.Toolbar;
 
-import com.uriahsolution.sms.R;
-import com.uriahsolution.sms.ui.timetable.TimeTableActivity;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.uriahsolution.homecare.R;
+
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.timetable_ll)
-    LinearLayout timeTableLinear;
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    private HashMap<String, String> url_maps;
+    @BindView(R.id.slider)
+    SliderLayout mDemoSlider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        timeTableLinear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, TimeTableActivity.class);
-                startActivity(intent);
+    }
+    private void setSlider() {
+        url_maps= new HashMap<String, String>();
+        url_maps.put("1","http://tvfiles.alphacoders.com/100/hdclearart-10.png");
 
-            }
-        });
+        url_maps.put("2", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+
+        url_maps.put("3","http://tvfiles.alphacoders.com/100/hdclearart-10.png");
 
 
+        mDemoSlider.removeAllSliders();
+        for(String name : url_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(this);
+            // initialize a SliderLayout
+            textSliderView
+
+                    .image(url_maps.get(name))
+
+                    .setScaleType(BaseSliderView.ScaleType.Fit);
+
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            /*textSliderView.getBundle()
+                    .putString("extra",name);*/
+
+            mDemoSlider.addSlider(textSliderView);
+        }
+        mDemoSlider.startAutoCycle();
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-    }
 }
--- app/src/main/java/com/uriahsolution/homecare/ui/main/MainContract.java
+++ app/src/main/java/com/uriahsolution/homecare/ui/main/MainContract.java
@@ -1,11 +1,11 @@
 package com.uriahsolution.homecare.ui.main;
 
-import com.uriahsolution.sms.BaseView;
 
+import com.uriahsolution.homecare.BaseView;
 
 public class MainContract {
 
-    interface View extends BaseView<MainContract.Presenter> {
+    interface View extends BaseView<Presenter> {
 
         void startActivity(Class<?> cls);
 
